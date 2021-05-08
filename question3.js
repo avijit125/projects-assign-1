886fbb373e5393f4c69f3adca51857bb681ffc2b
avijit125 <moupiyachanda1994@gmail.com>
2021-05-08 22:50:46 +0530

    did 4 questions

---
+++ question3.js
@@ -0,0 +1,21 @@
+const a = []  // give your input here
+
+function count_duplicate(a){
+ let counts = {}
+
+ for(let i =0; i < a.length; i++){ 
+     if (counts[a[i]]){
+     counts[a[i]] += 1
+     } else {
+     counts[a[i]] = 1
+     }
+    }  
+    for (let prop in counts){
+        if (counts[prop] >= 2){
+            console.log(prop + " counted: " + counts[prop] + " times.")
+        }
+    }
+  console.log(counts)
+}
+
+count_duplicate(a)
